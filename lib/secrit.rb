741f1f1d61ad5896b4481280698c6b9ba8f3d8fa
simpthy <141656492+simpthy@users.noreply.github.com>
2023-08-13 16:09:12 +0100

    Move password storage to instance variable
    We should be able to easily override the password storage location, both
    as a user and during testing. For this reason I'm moving it to an
    instance variable, which also required some reworking of the code so
    that tests could still run in parallel and so that even when using the
    `get` class method, we can still overrid this location if needed.

--- lib/secrit.rb
+++ lib/secrit.rb
@@ -1,20 +1,35 @@
 #!/usr/bin/env ruby
 require 'gpgme'
 
-PASSWORD_STORE_PATH = File.expand_path("~/.password-store/")
 
 class Secrit
-  def self.get(entry_path)
-    full_path = File.join(PASSWORD_STORE_PATH, "#{entry_path}.gpg")
+  attr_accessor :storage
 
-    # Ensure the file exists
+  def self.get(entry_path, storage: nil)
+    new(
+      storage: storage
+    ).get(entry_path).to_s.strip
+  end
+
+  # Allow optional injection of custom Decryptor
+  def initialize(decryptor: GPGDecryptor.new, storage: nil)
+    @decryptor = decryptor
+    @storage = storage || File.expand_path("~/.password-store/")
+  end
+
+  def get(entry_path)
+    full_path = construct_path(entry_path)
+    validate_file_existence(full_path)
+    @decryptor.decrypt(full_path)
+  end
+
+  private
+
+  def construct_path(entry_path)
+    File.join(storage, "#{entry_path}.gpg")
+  end
+
+  def validate_file_existence(full_path)
     raise "File not found: #{full_path}" unless File.exist?(full_path)
-
-    # Decrypt the file
-    crypto = GPGME::Crypto.new
-    decrypted_data = crypto.decrypt(File.open(full_path))
-
-    # Return the decrypted content
-    decrypted_data.to_s.strip
   end
 end
--- test/secrit_test.rb
+++ test/secrit_test.rb
@@ -4,6 +4,30 @@ require 'mocha/minitest'
 require_relative '../lib/secrit'
 
 class SecritTest < Minitest::Test
+  def with_encrypted_file(contents, key_path = 'test_entry')
+    Dir.mktmpdir do |temp_dir|
+      # Create a temporary GPG file with known content
+      test_file_path = File.join(temp_dir, "#{key_path}.gpg")
+      file = File.open(test_file_path, 'w+')
+
+      crypto = GPGME::Crypto.new
+      crypto.encrypt(contents, output: file)
+
+      yield(temp_dir)
+    end
+  end
+
+  def test_real_decryption
+    # Create a temporary GPG file with known content
+    key_path = 'foo_bar'
+
+    with_encrypted_file('test content', key_path) do |storage|
+      # Run the get method and check the result
+      result = Secrit.get(key_path, storage: storage)
+      assert_equal "test content", result
+    end
+  end
+
   def test_successful_decryption
     # Mocking the file existence check
     File.stubs(:exist?).returns(true)
@@ -21,12 +45,32 @@ class SecritTest < Minitest::Test
     assert_equal 'decrypted content', result
   end
 
+  def test_successful_decryption_object
+    entry_path = "valid_entry_path"
+    expected_decrypted_content = "secret content"
+
+    # Create a mock decryptor
+    mock_decryptor = Minitest::Mock.new
+    mock_decryptor.expect(:decrypt, expected_decrypted_content, [String])
+    File.stubs(:exist?).returns(true)
+
+    # Create an instance of Secrit with the mock decryptor
+    secrit = Secrit.new(decryptor: mock_decryptor)
+
+    # Test the get method
+    decrypted_content = secrit.get(entry_path)
+    assert_equal expected_decrypted_content, decrypted_content
+
+    # Verify that the mock expectations were met
+    mock_decryptor.verify
+  end
+
   def test_file_not_found
     # Mocking the file existence check to return false
     File.stubs(:exist?).returns(false)
 
     # Testing the get method to ensure it raises the correct error
-    assert_raises(RuntimeError, "File not found: #{File.join(PASSWORD_STORE_PATH, 'non_existent_file.gpg')}") do
+    assert_raises(RuntimeError, "File not found: #{File.join(Secrit.new.storage, 'non_existent_file.gpg')}") do
       Secrit.get('non_existent_file')
     end
   end
